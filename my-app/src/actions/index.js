9e8c095169917b184d605f338a4912a4e1c923c1
divyeshpuri <201652006@iiitvadodara.ac.in>
2019-09-14 02:23:46 +0530

    Completed Basic functionality :tada:

---
+++ my-app/src/actions/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export function getPosts() {
+  return function (dispatch) {
+    dispatch(requestPosts())
+    return axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      .then(json => {
+        if (json.status === 200) {
+          if (json) {
+            return dispatch(receivePost(json.data))
+          }
+          else {
+            console.log('Error in redux action')
+          }
+        }
+      })
+  }
+}
+
+export const requestPosts = () => {
+  return {
+    type: 'REQUEST_POST',
+    isFetching: true,
+    clicked: true
+  }
+}
+
+export const receivePost = (data) => {
+  return {
+    type: 'RECEIVE_POST',
+    isFetching: false,
+    posts: data,
+    clicked: false
+  }
+}
