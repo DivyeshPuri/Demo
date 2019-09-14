8b913f44f22b19f6661f712efcc48db7a663dcab
divyeshpuri <201652006@iiitvadodara.ac.in>
2019-09-14 15:08:41 +0530

    Updates

---
+++ precache-manifest.c08e0d773f8b7519ee79e44b06ace0bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b473a525aad9c4563b938e648737a4",
+    "url": "/Demo/index.html"
+  },
+  {
+    "revision": "06d951a491cf689ae9fc",
+    "url": "/Demo/static/css/main.d9894b04.chunk.css"
+  },
+  {
+    "revision": "496323bdd6aeafe18f8b",
+    "url": "/Demo/static/js/2.53e20867.chunk.js"
+  },
+  {
+    "revision": "06d951a491cf689ae9fc",
+    "url": "/Demo/static/js/main.d50f28c5.chunk.js"
+  },
+  {
+    "revision": "c797af85337edc2d2564",
+    "url": "/Demo/static/js/runtime~main.911ca91e.js"
+  }
+]);
